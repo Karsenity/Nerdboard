5e6a2212c205a8073b82354b70a70e94206e73ec
Karsenity <48289014+Karsenity@users.noreply.github.com>
2020-04-11 02:17:36 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect
+from src.app import app
 
-app = Flask(__name__)
-
-# Make the WSGI interface available at the top level so wfastcgi can get it.
-wsgi_app = app.wsgi_app
-
-#   from src.app import app
-
-@app.route('/')
-def home():
-    """Renders a Home page."""
-    return render_template("Hello")
- 
 if __name__=="__main__":
   app.run(debug=True)
