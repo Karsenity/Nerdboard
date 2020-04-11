e7f8a4ed08911df64f44203574465b187d3fa609
Karsenity <48289014+Karsenity@users.noreply.github.com>
2020-04-11 17:40:35 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
 from src.app import app
 
-import os
-HOST = os.environ.get('SERVER_HOST', 'localhost')
-try:
-    PORT = int(os.environ.get('SERVER_PORT', '5555'))
-except ValueError:
-    PORT = 5555
-app.run(HOST, PORT)
+if __name__ == "__main__": 
+    app.run()
