3d48bc501e1e97f96965c325e8b1b7ab517ead96
Karsenity <48289014+Karsenity@users.noreply.github.com>
2020-04-11 18:22:42 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.before_first_request
 def initialize_database():
     pass
-    #Database.initialize()
+    Database.initialize()
 
 
 uploads_dir = os.path.join(app.root_path, 'static/submissions/trailers')
@@ -20,5 +20,5 @@ def display_page():
     files = [f for f in os.listdir(uploads_dir) if f != '.DS_Store']
     video_urls = [url_for('static', filename='submissions/trailers/' + url) for url in files]
 
-    return render_template("display.html", len=len(video_urls), videos=video_urls)
+    #return render_template("display.html", len=len(video_urls), videos=video_urls)
     return "<h1>Welcome to Geeks for Geeks</h1>"
