9289d7f222eb9237a1cedff5237d0ea99560cf95
BuildTools <unconfigured@null.spigotmc.org>
2020-04-28 17:18:49 -0400

    Events working properly with Animation. Some styling remains to be done.

--- src/static/js/display.js
+++ src/static/js/display.js
@@ -1,5 +1,5 @@
+// VIDEO CAROUSEL
 $video_carousel = $('#myCarousel');
-
 
 $video_carousel.carousel({
     interval:30000
@@ -10,3 +10,19 @@ $video_carousel.on('slide.bs.carousel', function onSlide (ev) {
     vid.currentTime = 0;
     vid.play();
 });
+
+
+//EVENT BAR
+setInterval(function(){
+    setTimeout(function(){
+            $events = $('#events');
+            $event_cards = $events.find("div.card");
+            if ($event_cards.length > 4){
+                $event_cards[4].scrollIntoView({behavior: 'smooth', block: 'end'});
+            }
+        },
+        4500);
+        $events.append($event_cards[0]);
+    },
+    7000
+);
